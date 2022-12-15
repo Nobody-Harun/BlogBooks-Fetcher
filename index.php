63c239d229a6bbe92c86d0f8b5dc0c79e881cdea
local / BlogBooks <77278342+Nobody-Local@users.noreply.github.com>
2022-12-15 22:39:16 +0900

    a

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-    $url = "https://blogbooks.net/wp-json/wp/v2/posts?_fields=title,excerpt,author,link&per_page=100";
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, urlencode($url));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $html = curl_exec($ch);
-    var_dump($html);
-    curl_close($ch);
+    $url = "https://blogbooks.net/wp-json/wp/v2/posts?_fields=title,excerpt,author,link&per_page=".$_GET['json_number'];
+    var_dump(file_get_contents($url));
